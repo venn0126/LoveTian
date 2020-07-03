9c35b6eb4071d582ee8547c7eec1ae69ff5aae6c
venn <niuwei@fosafer.com>
2020-07-03 15:53:44 +0800

    test pod

---
+++ LoveTian/Classes/PrintGT.h
@@ -0,0 +1,19 @@
+//
+//  PrintGT.h
+//  TestPod
+//
+//  Created by Augus on 2020/7/3.
+//  Copyright © 2020 Fosafer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PrintGT : NSObject
+
+- (void)printGT;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LoveTian/Classes/PrintGT.m
@@ -0,0 +1,17 @@
+//
+//  PrintGT.m
+//  TestPod
+//
+//  Created by Augus on 2020/7/3.
+//  Copyright © 2020 Fosafer. All rights reserved.
+//
+
+#import "PrintGT.h"
+
+@implementation PrintGT
+
+- (void)printGT {
+    NSLog(@"niu love tian");
+}
+
+@end
